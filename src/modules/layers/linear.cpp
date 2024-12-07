d57a0b5b51fd60b3c91510379fa84d961fbbcd4c
lucaswychan <chanconan@gmail.com>
2024-12-08 00:20:37 +0800

    feat: linear backward

--- src/modules/layers/linear.cpp
+++ src/modules/layers/linear.cpp
@@ -1,5 +1,6 @@
-#include "modules/layers/linear.hpp"
-#include "utils/matrix_utils.hpp"
+#include "linear.hpp"
+#include <random>
+#include <cmath>
 using namespace nn;
 
 Linear::Linear(int in_features, int out_features, bool bias) : in_features_(in_features), out_features_(out_features), bias_(bias) {
@@ -11,6 +12,12 @@ Linear::Linear(int in_features, int out_features, bool bias) : in_features_(in_f
     else {
         this->biases_ = vector<vector<float>>();
     }
+
+    // randomize the weights. The bias is originally 0.
+    this->randomizeParams();
+
+    this->grad_weights_ = allocateMatrix(in_features, out_features);
+    this->grad_biases_ = allocateMatrix(out_features, 1);
 
     this->input_cache_ = vector<vector<float>>();
 }
@@ -35,17 +42,26 @@ vector<vector<float>> Linear::forward(const vector<vector<float>>& input) {
 }
 
 vector<vector<float>> Linear::backward(const vector<vector<float>>& grad_output) {
-    // grad_output = dL/dy
+    // grad_output = dL/dY
 
-    // dL/dW = X^T * dL/dy
+    // dL/dW = X^T * dL/dY
     this->grad_weights_ = matrixMultiplication(matrixTranspose(this->input_cache_), grad_output);
 
-    // dL/dX = dL/dy * W^T
+    // dL/dX = dL/dY * W^T
     vector<vector<float>> grad_input = matrixMultiplication(grad_output, matrixTranspose(this->weights_));
 
-    // dL/db = dL/dy^T * 1_B (1_B is a vector of ones of size batchSize)
-    if (this->bias_)
-        this->grad_biases_ = matrixMultiplication(matrixTranspose(grad_output), vector<vector<float>>(grad_output.size(), vector<float>(1, 1.0f)));
+    cout << "dL/dW: " << endl;
+    printMatrix(this->grad_weights_);
+    cout << endl;
+
+    // dL/db = dL/dY^T * 1_B (1_B is a vector of ones of size batchSize)
+    // dL/db = dL/dY.sum(axis=0)
+    if (this->bias_) 
+        this->grad_biases_ = matrixMultiplication(matrixTranspose(grad_output), allocateMatrix(grad_output.size(), 1, 1.0f));
+
+    cout << "dL/db: " << endl;
+    printMatrix(this->grad_biases_);
+    cout << endl;
 
     return grad_input;
 }
@@ -64,4 +80,21 @@ void Linear::update_params(const float lr) {
     }
 
     return;
+}
+
+void Linear::randomizeParams() {
+    // Calculate the limit for the uniform distribution
+    float limit = sqrt(6.0f / (this->in_features_ + this->out_features_));
+
+    // Set up the random number generator
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<float> dis(-limit, limit);
+
+    // Xavier initialization
+    for (int i = 0; i < this->in_features_; i++) {
+        for (int j = 0; j < this->out_features_; j++) {
+            this->weights_[i][j] = dis(gen);
+        }
+    }
 }
