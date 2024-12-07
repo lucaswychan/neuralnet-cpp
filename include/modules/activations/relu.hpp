6c300abeafb3f933f705edc317825f30eee9a7f4
lucaswychan <chanconan@gmail.com>
2024-12-08 00:19:34 +0800

    feat: relu template class

--- include/modules/activations/relu.hpp
+++ include/modules/activations/relu.hpp
@@ -1,1 +1,11 @@
 #pragma once
+#include "module.hpp"
+
+namespace nn {
+class ReLU : public Module {
+    public:
+        ReLU();
+        virtual vector<vector<float>> forward(const vector<vector<float>>& input) override;
+        virtual vector<vector<float>> backward(const vector<vector<float>>& grad_output) override;
+    };
+}
