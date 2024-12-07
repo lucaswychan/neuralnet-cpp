996c86a0e63bea9b1d850146eba04ed91a7e6111
lucaswychan <chanconan@gmail.com>
2024-12-08 00:20:02 +0800

    chore: remove freeMatrix

--- include/utils/matrix_utils.hpp
+++ include/utils/matrix_utils.hpp
@@ -1,8 +1,7 @@
 #include <vector>
 using namespace std;
 
-vector<vector<float>> allocateMatrix(int rows, int cols);
-void freeMatrix(vector<vector<float>>& matrix);
+vector<vector<float>> allocateMatrix(int rows, int cols, const float value = 0.0f);
 vector<vector<float>> matrixAddition(const vector<vector<float>>& A, const vector<vector<float>>& B);
 vector<vector<float>> matrixSubtraction(const vector<vector<float>>& A, const vector<vector<float>>& B);
 vector<vector<float>> matrixMultiplication(const vector<vector<float>>& A, const vector<vector<float>>& B);
