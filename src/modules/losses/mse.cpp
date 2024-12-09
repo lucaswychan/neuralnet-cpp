b3064fc333600412f05a09c2ccf7dd4d0db36a8e
lucaswychan <chanconan@gmail.com>
2024-12-10 00:36:18 +0800

    feat: add cross entropy loss

--- src/modules/losses/mse.cpp
+++ src/modules/losses/mse.cpp
@@ -3,7 +3,7 @@ using namespace nn;
 
 MSE::MSE() {}
 
-float MSE::forward(const Tensor<>& Y, const Tensor<>& Y_hat) {
+double MSE::forward(const Tensor<>& Y, const Tensor<>& Y_hat) {
     // Y R^B x M, Y_hat R^B x M , B is the batch size and M is the output dimension
 
     // 1 / (B * M) * ||(Y - Y_hat)||^2
@@ -19,14 +19,9 @@ float MSE::forward(const Tensor<>& Y, const Tensor<>& Y_hat) {
     Tensor<> diff = Y - Y_hat;
     diff *= diff;
 
-    float loss = 0.0f;
-    for (size_t i = 0; i < B; i++) {
-        for (size_t j = 0; j < M; j++) {
-            loss += diff[i, j];
-        }
-    }
+    double loss_without_factor = diff.sum();
 
-    return loss / (B * M);
+    return loss_without_factor / (B * M);
 }
 
 Tensor<> MSE::backward() {
@@ -35,7 +30,7 @@ Tensor<> MSE::backward() {
     // 2 / (B * M) * (Y - Y_hat)
 
     const size_t B = this->Y_cache_.shapes()[0], M = this->Y_cache_.shapes()[1];
-    const float factor = 2.0f / (B * M);
+    const double factor = 2.0f / (B * M);
 
     Tensor<> diff = this->Y_cache_ - this->Y_hat_cache_;
 
