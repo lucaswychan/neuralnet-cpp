759a2d9f27273509bd36b8eac71dfc5d09a49abf
lucaswychan <chanconan@gmail.com>
2025-01-09 13:28:46 +0800

    fix: correct to public inheritance

--- include/modules/losses/cross_entropy.hpp
+++ include/modules/losses/cross_entropy.hpp
@@ -3,11 +3,12 @@
 
 namespace nn {
 
-class CrossEntropyLoss : Loss {
+class CrossEntropyLoss : public Loss {
     public:
         CrossEntropyLoss();
         virtual double forward(const Tensor<>& Y_hat, const Tensor<>& Y) override;
         virtual Tensor<> backward() override;
+
     private:
         Tensor<> softmax_Y_hat_cache_;
     };
