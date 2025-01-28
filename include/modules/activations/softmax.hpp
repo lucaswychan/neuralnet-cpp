5e9ceacb893dc85bb34802acca276a66576c0d0b
lucaswychan <chanconan@gmail.com>
2025-01-28 22:45:41 +0800

    fix: add softmax_input_cache_ to cache the softmax version of input for backprop

--- include/modules/activations/softmax.hpp
+++ include/modules/activations/softmax.hpp
@@ -4,11 +4,14 @@
 namespace nn {
 class Softmax : public Module {
     private:
+        Tensor<> softmax_input_cache_;
+
         // Helper function to deal with multiple dimensions
         Tensor<> softmax_helper(const Tensor<>& input);
         vector<double> softmax_helper(const vector<double>& input);
     public:
         Tensor<> forward(const Tensor<>& input);
         Tensor<> backward(const Tensor<>& grad_output);
+        const Tensor<>& get_softmax_input_cache() const { return this->softmax_input_cache_; }
     };
 }
