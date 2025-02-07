852d5447a737fb8edafd840914cbbabf17a60d16
lucaswychan <chanconan@gmail.com>
2025-02-07 21:19:20 +0800

    finally I can start implementing conv2d

--- include/modules/layers/conv2d.hpp
+++ include/modules/layers/conv2d.hpp
@@ -5,7 +5,7 @@ namespace nn {
 
 class Conv2d : public Module {
     public:
-        Conv2d(int in_channels, int out_channels, int kernel_size, int stride = 1, int padding = 0);
+        Conv2d(int in_channels, int out_channels, int kernel_size, int stride = 1, int padding = 0, int dilation = 1, bool bias = true);
         virtual Tensor<> forward(const Tensor<>& input) override;
         virtual Tensor<> backward(const Tensor<>& grad_output) override;
         virtual void update_params(const float lr) override;
@@ -19,6 +19,8 @@ class Conv2d : public Module {
         int kernel_size_;
         int stride_;
         int padding_;
+        int dilation_;
+        bool bias_;
         Tensor<> weights_;
         Tensor<> biases_;
         Tensor<> grad_weights_;
--- src/modules/layers/conv2d.cpp
+++ src/modules/layers/conv2d.cpp
@@ -1,5 +1,12 @@
 #include "conv2d.hpp"
 using namespace nn;
 
-Conv2d::Conv2d(int in_channels, int out_channels, int kernel_size, int stride, int padding) {
+Conv2d::Conv2d(int in_channels, int out_channels, int kernel_size, int stride, int padding, int dilation, bool bias) {
+    this->in_channels_ = in_channels;
+    this->out_channels_ = out_channels;
+    this->kernel_size_ = kernel_size;
+    this->stride_ = stride;
+    this->padding_ = padding;
+    this->dilation_ = dilation;
+    this->bias_ = bias;
 }
