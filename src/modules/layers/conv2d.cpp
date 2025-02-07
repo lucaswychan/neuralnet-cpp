387507b6b65ee529ee4c6c1514c0427cc97d80dd
lucaswychan <chanconan@gmail.com>
2025-02-08 00:49:50 +0800

    temporarily remove the implementation

--- src/modules/layers/conv2d.cpp
+++ src/modules/layers/conv2d.cpp
@@ -2,11 +2,11 @@
 using namespace nn;
 
 Conv2d::Conv2d(int in_channels, int out_channels, int kernel_size, int stride, int padding, int dilation, bool bias) {
-    this->in_channels_ = in_channels;
-    this->out_channels_ = out_channels;
-    this->kernel_size_ = kernel_size;
-    this->stride_ = stride;
-    this->padding_ = padding;
-    this->dilation_ = dilation;
-    this->bias_ = bias;
+    // this->in_channels_ = in_channels;
+    // this->out_channels_ = out_channels;
+    // this->kernel_size_ = kernel_size;
+    // this->stride_ = stride;
+    // this->padding_ = padding;
+    // this->dilation_ = dilation;
+    // this->bias_ = bias;
 }
