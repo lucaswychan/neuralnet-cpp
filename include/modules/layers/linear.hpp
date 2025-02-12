160be1b1f905aa21c028febe81f48743d3fb1d2c
lucaswychan <chanconan@gmail.com>
2025-02-12 21:54:55 +0800

    refractor

--- include/modules/layers/linear.hpp
+++ include/modules/layers/linear.hpp
@@ -1,25 +1,27 @@
 #pragma once
 #include "module.hpp"
 
-namespace nn {
+namespace nn
+{
 
-class Linear : public Module{
+    class Linear : public Module
+    {
     public:
         Linear(size_t in_features, size_t out_features, bool bias);
-        
-        virtual Tensor<> forward(const Tensor<>& input) override;
-        virtual Tensor<> backward(const Tensor<>& grad_output) override;
+
+        virtual Tensor<> forward(const Tensor<> &input) override;
+        virtual Tensor<> backward(const Tensor<> &grad_output) override;
         virtual void update_params(const float lr) override;
 
         void randomizeParams();
 
         // setters
-        inline void set_weights(const Tensor<>& target_weight) { this->weight_ = target_weight; };
-        inline void set_biases(const Tensor<>& target_bias) { this->bias_ = target_bias; }
+        inline void set_weight(const Tensor<> &target_weight) { this->weight_ = target_weight; };
+        inline void set_bias(const Tensor<> &target_bias) { this->bias_ = target_bias; }
 
         // getters
-        inline const Tensor<>& getWeights() const { return this->weight_; }
-        inline const Tensor<>& getBiases() const { return this->bias_; }
+        inline const Tensor<> &get_weight() const { return this->weight_; }
+        inline const Tensor<> &get_bias() const { return this->bias_; }
 
     private:
         size_t in_features_;
@@ -29,6 +31,6 @@ class Linear : public Module{
         Tensor<> bias_;
         Tensor<> grad_weight_;
         Tensor<> grad_bias_;
-    };  
+    };
 
 }
