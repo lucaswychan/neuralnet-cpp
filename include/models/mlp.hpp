76255134c43569cde8e51157238cf65f50076030
lucaswychan <chanconan@gmail.com>
2025-05-17 12:20:25 +0800

    feat: add register_parameters to add the weights in an unordered_map for optimizer to update

--- include/models/mlp.hpp
+++ include/models/mlp.hpp
@@ -10,11 +10,18 @@ class MLP : public Module {
 
         virtual Tensor<> forward(const Tensor<>& input) override;
         virtual Tensor<> backward(const Tensor<>& grad_output) override;
-        virtual void update_params(const float lr) override;
         virtual Module& train(const bool mode = true) override;
         virtual Module& eval() override;
 
         inline const Sequential& get_layers() const { return this->layers_; }
+        
+        /**
+         * Get all parameters from the MLP model for optimization
+         */
+        virtual void register_parameters(
+            std::unordered_map<std::string, Tensor<>*>& params,
+            std::unordered_map<std::string, Tensor<>*>& grads,
+            const std::string& prefix = "") const override;
 
     private:
         Sequential layers_; // we use a sequential container to store the layers
